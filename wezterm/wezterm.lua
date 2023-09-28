3f1bd13dcfa9946f930e45b7df8b8fe10a40d4b0
Ethan Evans <ethanalexevans@gmail.com>
2023-09-28 11:02:57 -0700

    Add comment pointing to the source of my font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ end
 local home = 'C:/Users/ethan'
 local xdg_config_home = home .. '/.config'
 
-local nushell_config_home = xdg_config_home .. '/nushell' 
+local nushell_config_home = xdg_config_home .. '/nushell'
 config.default_prog = {
     'nu',
     '--login',
@@ -19,7 +19,7 @@ config.default_cwd = home
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font(
-    {
+    { -- https://tosche.net/fonts/comic-code
         family = 'Comic Code Ligatures',
         weight = 'DemiBold',
         italic = true,
